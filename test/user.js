1f0eb818aa1d88f999292df6e70c1ef8f3228b92
RaiRaiRobin <48660883+RaiRaiRobin@users.noreply.github.com>
2019-09-08 17:52:20 +0545

    admin login test

--- test/user.js
+++ test/user.js
@@ -21,7 +21,7 @@ after(done => {
 
 
 
-// Register
+// Register student
 describe('Users', function() {
     describe('POST user register', function() {
 
@@ -30,7 +30,6 @@ describe('Users', function() {
                 .post('/user/register/userFormData')
                 .send({
                     "FirstName": 'Jeson',
-                    "MiddleName": '',
                     "LastName": 'Rai',
                     "Gender": 'male',
                     "UserType": 'male',
@@ -59,6 +58,57 @@ describe('users', () => {
                 .post('/user/login')
                 .send({
                     "email": 'rai.rai.ribin1000@gmail.com',
+                    "password": 'asd',
+                })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    done();
+                });
+        });
+    });
+});
+
+
+// Register teacher
+describe('Users', function() {
+    describe('POST user register', function() {
+
+        it('it should register a single user', function(done) {
+            chai.request(myapp)
+                .post('/admin/register/teacher')
+                .send({
+                    "FirstName": 'Jeson',
+                    "LastName": 'Rai',
+                    "Gender": 'male',
+                    "UserType": 'male',
+                    "Address": 'Gokarneshwor',
+                    "DOB": '2019-06-17',
+                    "Email": 'rai.riiibin1000@gmail.com',
+                    "Phone": '+977-9817849333',
+                    "Bio": 'Teacher for 4 years',
+                    "Password": 'asd'
+                })
+                .end(function(err, res) {
+                    res.should.have.status(200);
+                    res.body.should.be.an('object');
+                    // res.body.should.have.property('message').eql('user data registered');
+                    done();
+                })
+        })
+    })
+})
+
+
+
+// admin login
+describe('admin login', () => {
+    describe('/POST login', () => {
+        it('it should log in admin', (done) => {
+            // chai.request(baseUrlRoutes)
+            chai.request(myapp)
+                .post('/admin/login')
+                .send({
+                    "email": 'rai.ribin1000@gmail.com',
                     "password": 'asd',
                 })
                 .end((err, res) => {
