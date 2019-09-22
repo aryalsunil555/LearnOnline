43114e41ddef47a6b04496ba5a844f9b5055f272
RaiRaiRobin <48660883+RaiRaiRobin@users.noreply.github.com>
2019-09-22 21:47:38 +0545

    error eliminated

--- app.js
+++ app.js
@@ -134,21 +134,9 @@ myapp.post('/teacher/login', authController.teachervalidator,authController.chec
 
 // admin login route
 myapp.post('/admin/login', authController.adminValidator, authController.checkPasswordMatch, authController.adminjwtTokenGen, function(req, res) {
-    // res.status(200);
     res.send({
         "status": 200,
         "message": "Admin logged in",
-<<<<<<< HEAD
-        // "token": req.genToken,
-        // "info": req.userInfo
-        "message": "Teacher logged in",
-        "token": req.genToken,
-        "info": req.userInfo
-=======
-        "token": req.genToken,
-        "info": req.userInfo
-
->>>>>>> ba9b3684a11ee69affd4d6c5c504efd1e06442d5
     })
 });
 
@@ -189,10 +177,6 @@ myapp.post('/admin/register', adminController.emailCheck, adminController.passwo
  })
 });
 
-<<<<<<< HEAD
-
-=======
->>>>>>> ba9b3684a11ee69affd4d6c5c504efd1e06442d5
 
 // admin login route
 myapp.post('/admin/login', authController.adminValidator,authController.checkPasswordMatch, authController.adminjwtTokenGen, function(req, res) {
@@ -203,10 +187,6 @@ myapp.post('/admin/login', authController.adminValidator,authController.checkPas
         "info": req.userInfo
     })
 });
-<<<<<<< HEAD
-=======
-
->>>>>>> ba9b3684a11ee69affd4d6c5c504efd1e06442d5
 
 
 //Course Register
@@ -250,12 +230,8 @@ myapp.get('/admin', function(req, res) {
     res.render('admin/admin');
 })
 
-<<<<<<< HEAD
 
-//get admin dashboard
-=======
 //get admin dashboard page
->>>>>>> ba9b3684a11ee69affd4d6c5c504efd1e06442d5
 myapp.get('/admindashboard', function(req, res) {
     res.render('admin/admindashboard');
 })
